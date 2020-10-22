d0ce7f346d2791968c37103641b22b5435a442ad
Alexandra Zaitsava <ilit_alex@tut.by>
2020-10-22 16:46:58 +0300

    add: new functional

--- src/App.js
+++ src/App.js
@@ -6,16 +6,65 @@ import Features from "./components/Features/Features";
 import Footer from "./components/Footer/Footer";
 import Calendar from "./components/Calendar/Calendar";
 import Details from "./components/Details/Details";
+import FetchData from "./service/FetchData";
 
 import './style.css'
 
 class App extends React.Component {
+    fetchData = new FetchData();
+    state = {
+        rocket: 'Falcon 1',
+        rocketFeatures: null,
+        rockets: [],
+        company: null,
+    };
+
+    componentDidMount() {
+        this.updateRocket();
+        this.updateCompany();
+
+    }
+
+
+
+    updateRocket () {
+        console.log('до', this.state);
+        this.fetchData.getRocket()
+            .then(data => {
+                this.setState( { rockets: data.map(item => item.name)})
+                return data
+            })
+            .then(data => data.find(item => item.name === this.state.rocket))
+            .then(rocketFeatures => {
+                  this.setState({rocketFeatures}, () => {console.log('после', this.state)});
+
+            });
+
+    };
+
+    changeRocket = rocket => {
+        this.setState({
+            rocket
+        }, this.updateRocket);
+    };
+
+
+    updateCompany = () => {
+        this.fetchData.getCompany()
+            .then(company => this.setState({ company }))
+    };
+
     render(){
+
+
         return (
+
             <React.Fragment>
-                <Header />
-                <Main />
-                <Features />
+                <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+                <Main rocket={this.state.rocket}/>
+                {this.state.rocketFeatures &&
+                    <Features {...this.state.rocketFeatures} />}
+                {this.state.company && <Footer {...this.state.company.links} />}
                 <Footer />
             </React.Fragment>
         )
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './header.css';
 import logo from "../../logo.svg";
 
-const Header = () => (
+
+
+const Header = props => (
     <header className="header">
         <img
             src={logo}
@@ -11,18 +13,19 @@ const Header = () => (
         />
         <nav className="main-nav nav">
             <ul className="list">
-                <li className="item">
-                    <a href="/" className="item-link">Falcon 1</a>
-                </li>
-                <li className="item">
-                    <a href="/" className="item-link">Falcon 9</a>
-                </li>
-                <li className="item">
-                    <a href="/" className="item-link">Falcon Heavy</a>
-                </li>
-                <li className="item">
-                    <a href="/" className="item-link">Updates</a>
-                </li>
+                {props.rockets.map((item, i) => (
+                    <li key={i} className="item">
+                        <a
+                            href="/"
+                            onClick={e =>{
+                                e.preventDefault();
+                                props.changeRocket(item)
+                            }}
+                            className="item-link"
+                        >{item}</a>
+                    </li>
+                ) )}
+
             </ul>
         </nav>
         <nav className="secondary-nav">
