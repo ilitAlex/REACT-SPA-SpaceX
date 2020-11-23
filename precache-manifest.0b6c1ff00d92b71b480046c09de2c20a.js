1814159ec208ba17654e4235d558c7a4d5bbce5a
Alexandra Zaitsava <ilit_alex@tut.by>
2020-11-23 23:30:10 +0300

    Updates

---
+++ precache-manifest.0b6c1ff00d92b71b480046c09de2c20a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17af9434855ef88aa4d4f13111415fe5",
+    "url": "/REACT-SPA-SpaceX/index.html"
+  },
+  {
+    "revision": "388f3c4bb316efe33f97",
+    "url": "/REACT-SPA-SpaceX/static/css/main.52e835e3.chunk.css"
+  },
+  {
+    "revision": "772d71abd5017f574d8a",
+    "url": "/REACT-SPA-SpaceX/static/js/2.b8af1e47.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/REACT-SPA-SpaceX/static/js/2.b8af1e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388f3c4bb316efe33f97",
+    "url": "/REACT-SPA-SpaceX/static/js/main.aa029966.chunk.js"
+  },
+  {
+    "revision": "b618f11d8c436a5c37a3",
+    "url": "/REACT-SPA-SpaceX/static/js/runtime-main.02d810cf.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/REACT-SPA-SpaceX/static/media/logo.d5ab31ce.svg"
+  }
+]);
